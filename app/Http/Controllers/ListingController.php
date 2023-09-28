a543894a58e81b472e4d971137788e04cece1007
psalmistcom <dadewale9@gmail.com>
2023-09-28 20:10:00 +0100

    layout structure with listing controller view

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
-    //
+    public function index()
+    {
+        $listings = Listing::where('is_active', true)
+            ->with('tags')
+            ->latest()
+            ->get();
+        // return $listings;
+        return view('listings.index', compact('listings'));
+    }
 }
